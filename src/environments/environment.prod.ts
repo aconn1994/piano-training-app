fe2dbac8faacc8121e2513fae058575d036d89a5
Adam Connolly <adam.m.connolly94@gmail.com>
2021-04-13 13:22:42 -0400

    deploy hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC_-dj73YBmh43V2E1Rl1MTdAC-6QVAFws",
+    authDomain: "piano-training-app.firebaseapp.com",
+    projectId: "piano-training-app",
+    storageBucket: "piano-training-app.appspot.com",
+    messagingSenderId: "512253664087",
+    appId: "1:512253664087:web:639a7fd417261c51937c12",
+    measurementId: "G-CCVN7GDN10"
+  }
 };
